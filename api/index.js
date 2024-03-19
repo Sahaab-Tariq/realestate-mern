10c0eff54c8aaceee86718bf1fd8678de833ced3
Sahaab-Tariq <sahabtariq888@gmail.com>
2024-03-19 20:41:30 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(() => {
+      console.log("Connected to MongoDB!");
+   })
+   .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+   });
+
 
 const app = express();
 
